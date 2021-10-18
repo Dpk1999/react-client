6f0576a31c540ae752e74c6aa856fc548f53739e
Dpk1999 <deepak.gaikwad@successive.tech>
2021-10-18 18:27:44 +0530

    feat:create components selectfield and radiogroup

--- src/components/TextField/style.js
+++ src/components/TextField/style.js
@@ -1,38 +1,50 @@
 export const error = {
   color: 'red',
   'margin-left': '5px',
+  'margin-right': '5px',
 };
+
 export const errorBorder = {
   border: '1px solid #f00',
   width: '100%',
   'margin-left': '5px',
+  'margin-right': '5px',
   padding: '12px 20px',
   display: 'inline-block',
   'border-radius': '4px',
   'box-sizing': 'border-box',
 };
+
 export const container = {
   width: '100%',
   display: 'flex',
   'flex-direction': 'column',
   border: '1px solid',
 };
+
 export const inputContainer = {
-  width: '100%',
+  width: '98%',
   display: 'flex',
+  'justify-content': 'center',
   'flex-direction': 'column',
   border: '1px solid',
   'margin-left': '5px',
+  'margin-top': '5px',
+  'padding-right': '10px',
 };
-export const input = {
+
+export const inputSize = {
   width: '100%',
-  'margin-left': '5px',
+  // 'margin-left': '5px',
+  // 'margin-right': '5px',
   padding: '12px 20px',
   display: 'inline-block',
   border: '1px solid #ccc',
   'border-radius': '4px',
   'box-sizing': 'border-box',
+  margin: '20px 0px 20px 0px',
 };
+
 export const headingStyle = {
   'margin-left': '5px',
 };
