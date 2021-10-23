09446c21cff7b940185a355de8a2c9b643a4bf7b
Dpk1999 <deepak.gaikwad@successive.tech>
2021-10-23 23:23:50 +0530

    fix:trainee component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import React from 'react';
-// import InputDemo from './pages/InputDemo';
-import AddDialog from './pages/Trainee/components/AddDialog';
-// import TextFieldDemo from './pages/TextFieldDemo/TextFieldDemo';
+import Trainee from './pages/Trainee/Trainee';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <TextFieldDemo /> */}
-        {/* <InputDemo /> */}
-        <AddDialog />
+        <Trainee />
       </header>
     </div>
   );
