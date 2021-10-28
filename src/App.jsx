22897606bdecb772393eab9063de0c3ae07b2e25
Dpk1999 <deepak.gaikwad@successive.tech>
2021-10-28 14:45:09 +0530

    feat:create traineelist and traineedetail

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import ChildrenDemo from './pages/ChildrenDemo';
 import Login from './pages/Login';
 import { AuthRoute, PrivateRoute } from './routes';
 import { NoMatch } from './pages';
+import TraineeDetail from './pages/Trainee/TraineeDetail';
 
 const App = () => (
   <>
@@ -24,6 +25,7 @@ const App = () => (
           <PrivateRoute exact path="/InputDemo" component={InputDemo} />
           <PrivateRoute exact path="/ChildrenDemo" component={ChildrenDemo} />
           <AuthRoute exact path="/Login" component={Login} />
+          <PrivateRoute path="/trainee/:id" component={TraineeDetail} />
           <PrivateRoute component={NoMatch} />
         </Switch>
       </ThemeProvider>
