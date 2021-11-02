1d34f250f09236660645bda13be62347045be021
Dpk1999 <deepak.gaikwad@successive.tech>
2021-11-02 12:04:13 +0530

    fix:resolve theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
-// import { ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import CssBaseline from '@mui/material/CssBaseline';
-// import theme from './theme';
 import Trainee from './pages/Trainee/Trainee';
 
 const App = () => (
