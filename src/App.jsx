937e2bdad77d7d7278dcd8cad1b638b5603c973d
Dpk1999 <deepak.gaikwad@successive.tech>
2021-11-02 11:54:46 +0530

    fix:resolve themeprovider

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,16 @@
 import React from 'react';
-import { ThemeProvider } from '@mui/material/styles';
+// import { ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
-// import CssBaseline from '@mui/material/CssBaseline';
-import theme from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
+// import theme from './theme';
 import Trainee from './pages/Trainee/Trainee';
 
 const App = () => (
   <>
-    {/* <CssBaseline /> */}
-    <ThemeProvider theme={theme}>
-      <Typography>
-        <Trainee />
-      </Typography>
-    </ThemeProvider>
+    <CssBaseline />
+    <Typography>
+      <Trainee />
+    </Typography>
   </>
 );
 
