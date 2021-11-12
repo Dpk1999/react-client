3db996910bcaebde8c1f1aaedc2764dcfdee1876
Dpk1999 <deepak.gaikwad@successive.tech>
2021-11-12 12:10:00 +0530

    adding sort and onClick event to table

--- src/components/Table/DataTable.jsx
+++ src/components/Table/DataTable.jsx
@@ -1,196 +1,86 @@
 import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import TableContainer from '@material-ui/core/TableContainer';
-import Table from '@material-ui/core/Table';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import TableCell from '@material-ui/core/TableCell';
-import TableSortLabel from '@mui/material/TableSortLabel';
-import TableBody from '@material-ui/core/TableBody';
-import Button from '@mui/material/Button';
-import { visuallyHidden } from '@mui/utils';
-
-const useStyles = makeStyles({
-  root: {
-    width: '100%',
-    marginTop: 18,
-  },
-  container: {
-    maxHeight: 440,
-  },
-});
-
-const StyledTableRow = withStyles((theme) => ({
-  root: {
-    '&:nth-of-type(odd)': {
-      backgroundColor: theme.palette.action.hover,
-    },
-    '&:hover': {
-      backgroundColor: 'rgb(223,223,223)',
-      cursor: 'pointer',
-    },
-  },
-}))(TableRow);
-
-const StyledTableCell = withStyles(() => ({
-  root: {
-    '&:hover': {
-      cursor: 'pointer',
-    },
-  },
-}))(TableCell);
-
-const ButtonLink = (props) => {
-  const { link, icon } = props;
-  return (
-    <Button
-      to={link}
-      variant="contained"
-      type="button"
-      size="small"
-      className="button-classes"
-      startIcon={icon}
-    />
-  );
-};
-
-const descendingComparator = (a, b, orderBy) => {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-};
-
-const getComparator = (order, orderBy) => (order === 'desc'
-  ? (a, b) => descendingComparator(a, b, orderBy)
-  : (a, b) => -descendingComparator(a, b, orderBy));
-
-const stableSort = (array, comparator) => {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) {
-      return order;
-    }
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-};
+import { Card, TableSortLabel } from '@mui/material';
+import { styled } from '@mui/system';
 
 const DataTable = (props) => {
   const {
-    id, data, columns, orderBy, order, onSort, compPath,
+    trainees, id, columns, order, orderBy, onSort,
   } = props;
-  const classes = useStyles();
 
-  const createSortHandler = (property) => (event) => {
-    onSort(event, property);
-  };
+  const StyledTableCell = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+      fontSize: '12px',
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(() => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: '#eeeeef',
+    },
+  }));
 
   return (
-    <Paper elevation={4} className={classes.root}>
-      <TableContainer className={classes.container}>
-        <Table id={id} stickyHeader aria-label="sticky table">
-          <TableHead onSort={onSort}>
-            <StyledTableRow>
-              {columns.map((header, index) => {
-                const { idx } = index;
-                return (
-                  <StyledTableCell
-                    align={header.align}
-                    style={
-                      {
-                        fontSize: 13,
-                        color: 'rgb(153, 153, 153)',
-                      }
-                    }
-                    key={idx}
-                    sortDirection={orderBy === header.field ? order : false}
-                  >
-                    <TableSortLabel
-                      active={orderBy === header.field}
-                      direction={orderBy === header.field ? order : 'asc'}
-                      onClick={createSortHandler(header.field)}
-                    >
-                      {header.label || 'field'}
-                      {orderBy === header.field ? (
-                        <Box component="span" sx={visuallyHidden}>
-                          {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
-                        </Box>
-                      ) : null}
-                    </TableSortLabel>
-                  </StyledTableCell>
-                );
-              })}
-            </StyledTableRow>
-          </TableHead>
-          <TableBody>
-            {stableSort(data, getComparator(order, orderBy)).map((item) => (
-              <StyledTableRow
-                key={item.id}
-                component={Link}
-                style={{ textDecoration: 'none' }}
-                to={`${compPath}/${item.id}`}
+    <TableContainer sx={{ width: 'auto', margin: '30px' }} component={Card}>
+      <Table
+        sx={{
+          '& .MuiTableRow-root:hover': {
+            backgroundColor: 'lightgray',
+            cursor: 'pointer',
+          },
+        }}
+      >
+        <TableHead>
+          {columns.map((row) => (
+            <StyledTableCell align={row.align} component="tr" scope="column">
+              <TableSortLabel
+                active={orderBy === row.field}
+                direction={order}
+                onClick={() => onSort(row.field)}
               >
-                {columns.map((body) => {
-                  const value = item[body.field];
-                  return (
-                    typeof body === 'object'
-                      ? (
-                        <StyledTableCell
-                          align={body.align}
-                          key={body}
-                        >
-                          {body.format && typeof value === 'string'
-                            ? body.format(value)
-                            : item[body.field]}
-                        </StyledTableCell>
-                      ) : (
-                        <TableCell align={body.align} key={body}>
-                          No data found
-                        </TableCell>
-                      )
-                  );
-                })}
-              </StyledTableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-    </Paper>
+                <span>{row.label}</span>
+              </TableSortLabel>
+            </StyledTableCell>
+          ))}
+        </TableHead>
+        <TableBody stripedRows id={id}>
+          {trainees.map((row) => (
+            <StyledTableRow
+              key={row.id}
+            >
+              {
+                Object.keys(row).map(
+                  (key) => (
+                    <StyledTableCell align={columns.find((column) => column.field === key).align}>
+                      {columns.find((column) => column.field === key).format(row[key])}
+                    </StyledTableCell>
+                  ),
+                )
+              }
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 };
 
-ButtonLink.propTypes = {
-  link: PropTypes.string.isRequired,
-  icon: PropTypes.string.isRequired,
-};
+export default DataTable;
 
 DataTable.propTypes = {
+  trainees: PropTypes.objectOf.isRequired,
   id: PropTypes.string.isRequired,
-  data: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    email: PropTypes.string.isRequired,
-    createdAt: PropTypes.string.isRequired,
-  })).isRequired,
-  columns: PropTypes.arrayOf(PropTypes.shape({
-    field: PropTypes.string.isRequired,
-    label: PropTypes.string,
-    align: PropTypes.string,
-    format: PropTypes.func,
-  })).isRequired,
+  columns: PropTypes.objectOf.isRequired,
+  order: PropTypes.string.isRequired,
   orderBy: PropTypes.string.isRequired,
-  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   onSort: PropTypes.func.isRequired,
-  compPath: PropTypes.string.isRequired,
 };
-
-export default DataTable;
--- src/pages/Trainee/components/index.js
+++ src/pages/Trainee/components/index.js
@@ -1,2 +1,1 @@
 export { default as AddDialog } from './AddDialog';
-export { default as TableList } from './TableList';
